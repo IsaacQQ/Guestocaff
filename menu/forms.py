8f743d9cc095a2e9f529774c4eb2774c4807ceb3
Игорь <74833446+IsaacQQ@users.noreply.github.com>
2021-01-14 00:25:31 +0200

    Update forms.py

--- menu/forms.py
+++ menu/forms.py
@@ -4,8 +4,9 @@ from restaurant.models import Category, Dish
 
 class CategoryForm(forms.ModelForm):
     title = forms.CharField(max_length=50,
-                                widget=forms.TextInput(attrs={'placeholder': "Назва категорії", 'required': "required"}))
-    category_order = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': "Порядок категорії у меню", 'required': "required"}))
+                            widget=forms.TextInput(attrs={'placeholder': "Назва категорії", 'required': "required"}))
+    category_order = forms.IntegerField(
+        widget=forms.TextInput(attrs={'placeholder': "Порядок категорії у меню", 'required': "required"}))
     is_visible = forms.BooleanField(initial=True, required=False)
 
     class Meta(object):
@@ -14,7 +15,6 @@ class CategoryForm(forms.ModelForm):
 
 
 class DishForm(forms.ModelForm):
-
     class Meta(object):
         model = Dish
         fields = ('title', 'price', 'photo', 'description', 'category')
