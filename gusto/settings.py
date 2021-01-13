fc8746a9f57f6e91c065f01acf10592dc1b809aa
Игорь <74833446+IsaacQQ@users.noreply.github.com>
2021-01-13 23:54:33 +0200

    Update settings.py

--- gusto/settings.py
+++ gusto/settings.py
@@ -143,6 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
